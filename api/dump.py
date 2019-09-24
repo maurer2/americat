811b0707579c8eefbc5575f481644a411c7771b1
Martin <mr.mmaurer+work@gmail.com>
2019-09-24 23:23:56 +0100

    Init

---
+++ api/dump.py
@@ -0,0 +1,8 @@
+from requests_html import HTMLSession
+session = HTMLSession()
+
+url = 'http://www.247wallst.com/special-report/2017/07/19/states-with-the-most-and-least-cat-owners/amp/'
+
+r = session.get(url)
+
+print(r.text)
