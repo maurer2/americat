50d4a4ba6973d97163854382d38dc8739c468f62
Martin <mr.mmaurer+work@gmail.com>
2019-09-28 23:04:16 +0100

    Added python linter

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Americat
 
 Issues
+Syntax Highlighting Sass:  
 Sass + vscode = https://github.com/UnwrittenFun/svelte-vscode/issues/1#issuecomment-495927947
--- api/dump.py
+++ api/dump.py
@@ -1,7 +1,8 @@
 from requests_html import HTMLSession
+
 session = HTMLSession()
 
-url = 'http://www.247wallst.com/special-report/2017/07/19/states-with-the-most-and-least-cat-owners/amp/'
+url = "http://www.247wallst.com/special-report/2017/07/19/states-with-the-most-and-least-cat-owners/amp/"
 
 r = session.get(url)
 
