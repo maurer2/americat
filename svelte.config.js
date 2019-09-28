1f76cad00932891d6540c61e6fb33b750eebbb6f
Martin <mr.mmaurer+work@gmail.com>
2019-09-28 22:39:17 +0100

    Cleaned up svelte config
    Fixed config again

--- svelte.config.js
+++ svelte.config.js
@@ -4,25 +4,28 @@ const sass = require('node-sass');
 module.exports = {
   preprocess: {
     style: async ({ content, attributes }) => {
-      if (attributes.type !== 'text/scss' && attributes.lang !== 'scss') return; // lang is now taken into account
-      
+      if (attributes.type !== 'text/scss' && attributes.lang !== 'scss') {
+        return false;
+      }
+
       return new Promise((resolve, reject) => {
-        sass.render(
-          {
-            data: content,
-            sourceMap: true,
-            outFile: 'x', // this is necessary, but is ignored
-          },
-          (err, result) => {
-            if (err) return reject(err);
-            
-            resolve({
-              code: result.css.toString(),
-              map: result.map.toString(),
-            });
-          },
-        );
+        sass.render({
+          data: content,
+          sourceMap: true,
+          outFile: 'x', // this is necessary, but is ignored
+        }, (error, result) => {
+          if (error) {
+            return reject(error);
+          }
+
+          const { css: code, map: sourceMap } = result;
+
+          return resolve({
+            code: code.toString(),
+            map: sourceMap.toString(),
+          });
+        });
       });
     },
   },
-}
+};
