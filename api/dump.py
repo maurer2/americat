4c239a422b7ffd91e3b918197e76369ce62e8af7
Martin <mr.mmaurer+work@gmail.com>
2019-09-28 23:57:47 +0100

    Added content dump function

--- api/dump.py
+++ api/dump.py
@@ -1,9 +1,32 @@
+"""
+dump website body content to file
+"""
+
+import os
+import sys
+
 from requests_html import HTMLSession
 
-session = HTMLSession()
+# pylint: disable=line-too-long
+URL = "http://www.247wallst.com/special-report/2017/07/19/states-with-the-most-and-least-cat-owners/amp/"
 
-url = "http://www.247wallst.com/special-report/2017/07/19/states-with-the-most-and-least-cat-owners/amp/"
 
-r = session.get(url)
+def get_markup(link: str) -> str:
+    """Fetch markup"""
+    session = HTMLSession()
+    url_response = session.get(link)
 
-print(r.text)
+    return url_response.text
+
+
+def write_file(text: str) -> None:
+    """Write to file"""
+    path = os.path.join(sys.path[0], "dump.txt")
+    file = open(path, "w")
+    file.write(text)
+    file.close()
+
+
+MARKUP = get_markup(URL)
+
+write_file(MARKUP)
