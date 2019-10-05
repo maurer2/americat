40665106ada040a31d6cd318f29c9a8da2f58e46
Martin <mr.mmaurer+work@gmail.com>
2019-10-05 23:50:10 +0100

    Added some list rendering

--- src/components/Results/Results.svelte
+++ src/components/Results/Results.svelte
@@ -4,14 +4,55 @@
 </script>
 
 <style lang="scss">
+  .list {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  .entry {
+    display: flex;
+    padding: 0;
+    flex-direction: column;
+    list-style: none;
+
+    & + & {
+      margin-top: 1rem;
+    }
+  }
+
+  .detail {
+    display: flex;
+    margin: 0;
+  }
+
+  .key {
+    font-weight: bold;
+
+    &::after {
+      content: ":";
+      color: bold;
+    }
+  }
+  
+  .value {
+    margin-left: 0.25rem;
+  }
 </style>
 
 <template lang="html">
-  Results { list.length }
-
-  <code>
-    <pre>
-      { JSON.stringify(list, null, 2) }
-    </pre>
-  </code>
+  <ul class="list">
+    {#each list as entry, i}
+      <li class="entry" key="{i}">
+        <dl class="detail">
+          <dt class="key">Rank</dt>
+          <dt class="value">{entry.rank}</dt>
+        </dl>
+        <dl class="detail">
+          <dt class="key">State</dt>
+          <dt class="value">{entry.state}</dt>
+        </dl>
+      </li>
+    {/each}
+  </ul>
 </template>
