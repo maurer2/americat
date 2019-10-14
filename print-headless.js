1c93325b8c556f9c5b03886a8aebe3692fdafc13
Martin <mr.mmaurer+work@gmail.com>
2019-10-14 23:37:26 +0100

    Added print export via chrome headless

---
+++ print-headless.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://127.0.0.1:8080/?print-pdf', { waitUntil: 'networkidle2' });
+  await page.emulateMedia('screen');
+  await page.pdf({
+    path: 'slides.pdf',
+    format: 'A4',
+    landscape: true,
+    printBackground: true,
+    // width: '297mm',
+    // height: '210mm',
+  });
+
+  await browser.close();
+})();
